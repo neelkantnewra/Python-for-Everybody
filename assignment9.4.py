48acf4be3b2bb27f5b17c40cbf1f4217ec594665
Neelkant Newra <63470232+neelkantnewra@users.noreply.github.com>
2020-05-31 16:59:23 +0530

    Create assignment9.4.py

---
+++ assignment9.4.py
@@ -0,0 +1,24 @@
+'''9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.'''
+
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+lst = list()
+dic = dict()
+for line in handle:
+    if line.startswith("From"):
+        if line.startswith("From:"):
+            continue
+        else:
+            z = line.split()
+            lst.append(z[1])
+for i in lst:
+    dic[i] = lst.count(i)
+
+val =max(dic.values())
+    
+for k,v in dic.items():
+    if v == val:
+        print(k,v)
