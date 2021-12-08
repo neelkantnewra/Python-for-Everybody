a9cd956835199242c1b6206373cfbb9c0a58b044
Neelkant Newra <newra008@gmail.com>
2021-12-08 17:48:20 +0530

    Create linearsearch.py

---
+++ Competition/linearsearch.py
@@ -0,0 +1,20 @@
+# Author: Neelkant Newra
+# GitHub: https://github.com/neelkantnewra
+
+def LinearSearch(lst,n):
+    '''
+    lst: sorted array
+    n: number to be find
+    return(bool) True/False 
+    '''
+    for i in range(len(lst)):
+        if lst[i]==n:
+            return True
+    return False
+    
+def main():
+    lst = [1,2,3,4,5,6,7,8,9,10,23,34,45,55,667,888,9999]
+    n = 888
+    print(LinearSearch(lst,n))
+    
+main()
